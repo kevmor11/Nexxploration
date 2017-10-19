409845851056cc9022aff1a78a68edaf17294d3c
kevmor11 <morissette.kevin@gmail.com>
2017-10-18 19:37:26 -0700

    Refactoring

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const express = require('express'),
       router = express.Router()
 
 .get('/', (req, res) => {
-  request('https://launchlibrary.net/1.2/launch/next/30', (error, response, body) => {
+  request('https://launchlibrary.net/1.2/launch/next/80', (error, response, body) => {
     const launches = JSON.parse(body).launches;
     res.render('index', { launches });
   });
