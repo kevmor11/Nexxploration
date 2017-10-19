6c6ddfa25a3c96c7e5d889b405bfee692c50df98
kevmor11 <morissette.kevin@gmail.com>
2017-10-18 17:32:26 -0700

    Styling

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,24 @@
-require('dotenv').load();
-
 const express = require('express'),
       request = require('request'),
-      API = process.env.API,
+      API = 'https://launchlibrary.net/1.2',
       router = express.Router()
 
-/* GET home page. */
 .get('/', (req, res) => {
-  res.render('index', { title: 'Express' });
+  request(`${API}/launch/next/30`, (error, response, body) => {
+    const launches = JSON.parse(body).launches;
+    console.log('error:', error);
+    console.log('body:', launches[0]);
+    res.render('index', { launches });
+  });
 });
 
-function queryAPI(parameter) {
-  request(`${API}/${parameter}`, (error, response, body) => {
-    console.log('error:', error); // Print the error if one occurred
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    console.log('body:', body); // Print the HTML for the Google homepage.
-  });
-}
-
-queryAPI('agency');
+// function queryAPI(parameter) {
+//   request(`${API}/${parameter}`, (error, response, body) => {
+//     console.log('error:', error); // Print the error if one occurred
+//     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+//     console.log('body:', body); // Print the HTML for the Google homepage.
+//     return body;
+//   });
+// }
 
 module.exports = router;
