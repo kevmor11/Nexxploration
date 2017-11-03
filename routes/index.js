03ac5c5e26adf2f5b16583734c0e631005114937
kevmor11 <morissette.kevin@gmail.com>
2017-11-03 12:45:36 -0700

    Error handling

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,13 @@ const express = require('express'),
       router = express.Router()
 
 .get('/', (req, res) => {
-  request('https://launchlibrary.net/1.2/launch/next/80', (error, response, body) => {
+  request('https://launchlibrary.net/1.2/launch/next/80', (err, response, body) => {
+    if(err) return res.end(err.message);
     const launches = JSON.parse(body).launches;
     res.render('index', { launches });
-  });
+  }).on('error', (e) => {
+    console.log(e)
+  }).end();
 });
 
 module.exports = router;
