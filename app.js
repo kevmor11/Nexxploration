6fc969cbde2d1d296eff27932d7d1c8f874ad939
kevmor11 <morissette.kevin@gmail.com>
2017-11-03 12:47:03 -0700

    compression config

--- app.js
+++ app.js
@@ -15,13 +15,13 @@ const express = require('express'),
 .set('views', path.join(__dirname, 'views'))
 .set('view engine', 'ejs')
 
+.use(compression())
 .use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 .use(logger('dev'))
 .use(bodyParser.json())
 .use(bodyParser.urlencoded({ extended: false }))
 .use(cookieParser())
 .use(express.static(path.join(__dirname, 'public')))
-.use(compression())
 
 .use('/', index)
 
